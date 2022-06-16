cb4f78021f162455bfa33781b0a0d5496cc7cd14
Martin Stamm <Martin.Stamm@eilebrecht.de>
2022-06-16 12:55:19 +0200

    new class vorlesungen

--- app/src/main/java/com/example/raplaapp/MainActivity.java
+++ app/src/main/java/com/example/raplaapp/MainActivity.java
@@ -3,11 +3,21 @@ package com.example.raplaapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Map<Integer, Vorlesung> Vorlesungen = new HashMap<>()
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    private void parseIncommingStringInList(String inCommingString){
+            
     }
 }
