957766e4e267a05c4d51cd552bc652076a1efdf0
Martin Stamm <Martin.Stamm@eilebrecht.de>
2022-06-16 19:58:36 +0200

    implemented method parsing data out of string from web.

--- app/src/main/java/com/example/raplaapp/MainActivity.java
+++ app/src/main/java/com/example/raplaapp/MainActivity.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Map<Integer, Vorlesung> Vorlesungen = new HashMap<>()
+    public Map<Integer, Vorlesung> Vorlesungen = new HashMap<>()
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +18,156 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void parseIncommingStringInList(String inCommingString){
-            
+        String[] inhaltSplitted = inCommingString.split("\n");
+
+        String dateMo;
+        String dateDi;
+        String dateMi;
+        String dateDo;
+        String dateFr;
+
+        //jede zeile aus dem web auf eine Vorlesung überprüfen, und Vorlesung erstellen
+        for (int lineOfCodeFromWeb = 1; lineOfCodeFromWeb < inhaltSplitted.length; lineOfCodeFromWeb++) {
+            //extract dates
+            if(inhaltSplitted[n].contains("</th><td class=\"week_header\"")){
+                String weedate = inhaltSplitted[n];
+
+                String week = weedate.split("</th><td class=\"week_header\"")[0];
+                week = week.split("class=\"week_number\">")[1];
+
+                dateMo = weekdate.split(".</nobr>")[0];
+                dateDi = weekdate.split(".</nobr>")[1];
+                dateMi = weekdate.split(".</nobr>")[2];
+                dateDo = weekdate.split(".</nobr>")[3];
+                dateFr = weekdate.split(".</nobr>")[4];
+
+                dateMo = dateMo.strip();
+                dateDi = dateDi.strip();
+                dateMi = dateMi.strip();
+                dateDo = dateDo.strip();
+                dateFr = dateFr.strip();
+            }
+
+            //parsing Time and Name
+            if(inhaltSplitted[n].contains("[ Teiln]")){
+                String weekday;
+                String substring = inhaltSplitted[n].split("[ Teiln]")[0];
+                String name = substring.rsplit("<br/>")[1];
+                substring = substring.rsplit("<br/>")[0];
+
+                String endtime = substring.rsplit("&#160;-")[1];
+                substring = substring.rsplit("&#160;-")[0];
+
+                String starttime = "";
+
+                if(substring.contains('<a href="#0">')){
+                    starttime = substring.rsplit('<a href="#0">')[1]
+                    substring = substring.rsplit('<a href="#0">')[0]
+                }
+
+                if(substring.contains('<a href="#1">')){
+                    starttime = substring.rsplit('<a href="#1">')[1]
+                    substring = substring.rsplit('<a href="#1">')[0]
+                }
+
+                if(substring.contains('<a href="#2">')){
+                    starttime = substring.rsplit('<a href="#2">')[1]
+                    substring = substring.rsplit('<a href="#2">')[0]
+                }
+
+                if(substring.contains('<a href="#3">')){
+                    starttime = substring.rsplit('<a href="#3">')[1]
+                    substring = substring.rsplit('<a href="#3">')[0]
+                }
+
+                if(substring.contains('<a href="#4">')) {
+                    starttime = substring.rsplit('<a href="#4">')[1]
+                    substring = substring.rsplit('<a href="#4">')[0]
+                }
+
+                if(substring.contains('<a href="#5">')){
+                    starttime = substring.rsplit('<a href="#5">')[1]
+                    substring = substring.rsplit('<a href="#5">')[0]
+                }
+
+                if(substring.contains('<a href="#6">')){
+                    starttime = substring.rsplit('<a href="#6">')[1]
+                    substring = substring.rsplit('<a href="#6">')[0]
+                }
+
+                if(substring.contains('<a href="#7">')){
+                    starttime = substring.rsplit('<a href="#7">')[1]
+                    substring = substring.rsplit('<a href="#7">')[0]
+                }
+
+                if(substring.contains('<a href="#8">')){
+                    starttime = substring.rsplit('<a href="#8">')[1]
+                    substring = substring.rsplit('<a href="#8">')[0]
+                }
+
+                if(substring.contains('<a href="#9">')){
+                    starttime = substring.rsplit('<a href="#9">')[1]
+                    substring = substring.rsplit('<a href="#9">')[0]
+                }
+
+                if(substring.contains('<a href="#10">')){
+                    starttime = substring.rsplit('<a href="#10">')[1]
+                    substring = substring.rsplit('<a href="#10">')[0]
+                }
+
+                String substringDate = inhaltSplitted[n+1];
+                String date = substringDate.split("</div>")[0];
+                date = date.split("</div>")[1];
+
+                //parsing date
+                if(!date.contains("wöchentlich")){
+                    date = date.split(" ")[1];
+                    date = date.split(" ")[0];
+
+                    if(date.contains(".22")){
+                        date = date.split(".22")[0];
+                    }
+                    else if(date.contains(".23")){
+                        date = date.split(".23")[0];
+                    }
+                    else if(date.contains(".24")){
+                        date = date.split(".24")[0];
+                    }
+                }else {
+                    if (date.contains("Mo")){
+                        date = dateMo;
+                    } else if (date.contains("Di")) {
+                        date = dateDi;
+                    } else if (date.contains("Mi")) {
+                        date = dateMi;
+                    } else if (date.contains("Do")) {
+                        date = dateDo;
+                    } else if (date.contains("Fr")) {
+                        date = dateFr;
+                    }
+                }
+
+                //parsing weekday
+                if(date.equals(dateMo)){
+                    weekday = "Mo";
+                } else if (date.equals(dateDi)) {
+                    weekday = "Di";
+                } else if (date.equals(dateMi)) {
+                    weekday = "Mi";
+                } else if (date.equals(dateDo)) {
+                    weekday = "Do";
+                } else if (date.equals(dateFr)) {
+                    weekday = "Fr";
+                }
+
+                String place = substringDate.split("</div>")[1];
+                place = place.split("<strong>")[1];
+                place = place.split("</strong>")[0];
+
+                vorlesungen.put(vorlesungen.size()+1, new vorlesung(weekday, name, place, date, starttime, endtime));
+
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/raplaapp/Vorlesung.java
+++ app/src/main/java/com/example/raplaapp/Vorlesung.java
@@ -2,12 +2,26 @@ import jdk.tools.jmod.Main;
 
 public class Vorlesung extends MainActivity {
     
+    private String weekday;
     private String name;
     private String ort;
     private String datum;
-    private Time startzeit;
-    private Time endzeit;
+    private String startzeit;
+    private String endzeit;
+    
+    public Vorlesung(String weekday, String name, String place, String date, String startdate, String endDate){
+        setWeekday(weekday);
+        setName(name.trim());
+        setOrt(place);
+        setDatum(date);
+        setStartzeit(startdate);
+        setEndzeit(endzeit);
+    }
 
+    public void setWeekday(String weekday){
+        this.weekday = weekday;
+    }
+    
     public void setName(String name) {
         this.name = name;
     }
@@ -20,14 +34,18 @@ public class Vorlesung extends MainActivity {
         this.datum = datum;
     }
 
-    public void setStartzeit(Time startzeit) {
+    public void setStartzeit(String startzeit) {
         this.startzeit = startzeit;
     }
 
-    public void setEndzeit(Time endzeit) {
+    public void setEndzeit(String endzeit) {
         this.endzeit = endzeit;
     }
 
+    public  String getWeekday() {
+        return weekday;
+    }
+    
     public String getName() {
         return name;
     }
@@ -40,11 +58,11 @@ public class Vorlesung extends MainActivity {
         return datum;
     }
 
-    public Time getStartzeit() {
+    public String getStartzeit() {
         return startzeit;
     }
 
-    public Time getEndzeit() {
+    public String getEndzeit() {
         return endzeit;
     }
 }
