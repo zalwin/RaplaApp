1c18d8b60ffe7567438ed47d144d24dbfecd65f1
Martin Stamm <Martin.Stamm@eilebrecht.de>
2022-06-19 14:06:58 +0200

    ich hab das nochmal überarbeitet

--- app/src/main/java/com/example/raplaapp/MainActivity.java
+++ app/src/main/java/com/example/raplaapp/MainActivity.java
@@ -17,29 +17,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void parseIncommingStringInList(String inCommingString){
+    private static void parseIncommingStringInList(String inCommingString) {
         String[] inhaltSplitted = inCommingString.split("\n");
 
-        String dateMo;
-        String dateDi;
-        String dateMi;
-        String dateDo;
-        String dateFr;
+        String dateMo = null;
+        String dateDi = null;
+        String dateMi = null;
+        String dateDo = null;
+        String dateFr = null;
+        String weekday = "";
 
         //jede zeile aus dem web auf eine Vorlesung überprüfen, und Vorlesung erstellen
         for (int lineOfCodeFromWeb = 1; lineOfCodeFromWeb < inhaltSplitted.length; lineOfCodeFromWeb++) {
             //extract dates
-            if(inhaltSplitted[n].contains("</th><td class=\"week_header\"")){
-                String weedate = inhaltSplitted[n];
+            if (inhaltSplitted[lineOfCodeFromWeb].contains("</th><td class=\"week_header\"")) {
+                String weekdate = inhaltSplitted[lineOfCodeFromWeb];
 
-                String week = weedate.split("</th><td class=\"week_header\"")[0];
+                String week = weekdate.split("</th><td class=\"week_header\"")[0];
                 week = week.split("class=\"week_number\">")[1];
 
-                dateMo = weekdate.split(".</nobr>")[0];
-                dateDi = weekdate.split(".</nobr>")[1];
-                dateMi = weekdate.split(".</nobr>")[2];
-                dateDo = weekdate.split(".</nobr>")[3];
-                dateFr = weekdate.split(".</nobr>")[4];
+                dateMo = weekdate.split("\\.</nobr>")[0];
+                dateDi = weekdate.split("\\.</nobr>")[1];
+                dateMi = weekdate.split("\\.</nobr>")[2];
+                dateDo = weekdate.split("\\.</nobr>")[3];
+                dateFr = weekdate.split("\\.</nobr>")[4];
+
+                dateMo = dateMo.split("<nobr>")[1];
+                dateDi = dateDi.split("<nobr>")[1];
+                dateMi = dateMi.split("<nobr>")[1];
+                dateDo = dateDo.split("<nobr>")[1];
+                dateFr = dateFr.split("<nobr>")[1];
+
+                dateMo = dateMo.split(" ")[1];
+                dateDi = dateDi.split(" ")[1];
+                dateMi = dateMi.split(" ")[1];
+                dateDo = dateDo.split(" ")[1];
+                dateFr = dateFr.split(" ")[1];
+
+                System.out.println(dateMo);
+                System.out.println(dateDi);
+                System.out.println(dateMi);
+                System.out.println(dateDo);
+                System.out.println(dateFr);
 
                 dateMo = dateMo.strip();
                 dateDi = dateDi.strip();
@@ -49,92 +68,108 @@ public class MainActivity extends AppCompatActivity {
             }
 
             //parsing Time and Name
-            if(inhaltSplitted[n].contains("[ Teiln]")){
-                String weekday;
-                String substring = inhaltSplitted[n].split("[ Teiln]")[0];
-                String name = substring.rsplit("<br/>")[1];
-                substring = substring.rsplit("<br/>")[0];
+            if (inhaltSplitted[lineOfCodeFromWeb].contains("[ Teiln]")) {
 
-                String endtime = substring.rsplit("&#160;-")[1];
-                substring = substring.rsplit("&#160;-")[0];
+                //System.out.println(inhaltSplitted[lineOfCodeFromWeb]);
+
+                String stringToSplit = inhaltSplitted[lineOfCodeFromWeb];
+
+                String[] substrings = stringToSplit.split("\\[ Teiln]");   //[0]  [ Teiln]
+
+//                for (int i = 0; i < substrings.length; i++) {
+//                    System.out.println(substrings[i]);
+//                }
+
+                String substring = substrings[0];
+
+                //System.out.println("\n" + substring);
+                String name = substring.split("<br/>")[1];
+                substring = substring.split("<br/>")[0];
+
+                String endtime = substring.split("&#160;-")[1];
+                substring = substring.split("&#160;-")[0];
 
                 String starttime = "";
 
-                if(substring.contains('<a href="#0">')){
-                    starttime = substring.rsplit('<a href="#0">')[1]
-                    substring = substring.rsplit('<a href="#0">')[0]
+                if (substring.contains("<a href=\"#0\">")) {
+                    starttime = substring.split("<a href=\"#0\">")[1];
+                    substring = substring.split("<a href=\"#0\">")[0];
                 }
 
-                if(substring.contains('<a href="#1">')){
-                    starttime = substring.rsplit('<a href="#1">')[1]
-                    substring = substring.rsplit('<a href="#1">')[0]
+                if (substring.contains("<a href=\"#1\">")) {
+                    starttime = substring.split("<a href=\"#1\">")[1];
+                    substring = substring.split("<a href=\"#1\">")[0];
                 }
 
-                if(substring.contains('<a href="#2">')){
-                    starttime = substring.rsplit('<a href="#2">')[1]
-                    substring = substring.rsplit('<a href="#2">')[0]
+                if (substring.contains("<a href=\"#2\">")) {
+                    starttime = substring.split("<a href=\"#2\">")[1];
+                    substring = substring.split("<a href=\"#2\">")[0];
                 }
 
-                if(substring.contains('<a href="#3">')){
-                    starttime = substring.rsplit('<a href="#3">')[1]
-                    substring = substring.rsplit('<a href="#3">')[0]
+                if (substring.contains("<a href=\"#3\">")) {
+                    starttime = substring.split("<a href=\"#3\">")[1];
+                    substring = substring.split("<a href=\"#3\">")[0];
                 }
 
-                if(substring.contains('<a href="#4">')) {
-                    starttime = substring.rsplit('<a href="#4">')[1]
-                    substring = substring.rsplit('<a href="#4">')[0]
+                if (substring.contains("<a href=\"#4\">")) {
+                    starttime = substring.split("<a href=\"#4\">")[1];
+                    substring = substring.split("<a href=\"#4\">")[0];
                 }
 
-                if(substring.contains('<a href="#5">')){
-                    starttime = substring.rsplit('<a href="#5">')[1]
-                    substring = substring.rsplit('<a href="#5">')[0]
+                if (substring.contains("<a href=\"#5\">")) {
+                    starttime = substring.split("<a href=\"#5\">")[1];
+                    substring = substring.split("<a href=\"#5\">")[0];
                 }
 
-                if(substring.contains('<a href="#6">')){
-                    starttime = substring.rsplit('<a href="#6">')[1]
-                    substring = substring.rsplit('<a href="#6">')[0]
+                if (substring.contains("<a href=\"#6\">")) {
+                    starttime = substring.split("<a href=\"#6\">")[1];
+                    substring = substring.split("<a href=\"#6\">")[0];
                 }
 
-                if(substring.contains('<a href="#7">')){
-                    starttime = substring.rsplit('<a href="#7">')[1]
-                    substring = substring.rsplit('<a href="#7">')[0]
+                if (substring.contains("<a href=\"#7\">")) {
+                    starttime = substring.split("<a href=\"#7\">")[1];
+                    substring = substring.split("<a href=\"#7\">")[0];
                 }
 
-                if(substring.contains('<a href="#8">')){
-                    starttime = substring.rsplit('<a href="#8">')[1]
-                    substring = substring.rsplit('<a href="#8">')[0]
+                if (substring.contains("<a href=\"#8\">")) {
+                    starttime = substring.split("<a href=\"#8\">")[1];
+                    substring = substring.split("<a href=\"#8\">")[0];
                 }
 
-                if(substring.contains('<a href="#9">')){
-                    starttime = substring.rsplit('<a href="#9">')[1]
-                    substring = substring.rsplit('<a href="#9">')[0]
+                if (substring.contains("<a href=\"#9\">")) {
+                    starttime = substring.split("<a href=\"#9\">")[1];
+                    substring = substring.split("<a href=\"#9\">")[0];
                 }
 
-                if(substring.contains('<a href="#10">')){
-                    starttime = substring.rsplit('<a href="#10">')[1]
-                    substring = substring.rsplit('<a href="#10">')[0]
+                if (substring.contains("<a href=\"#10\">")) {
+                    starttime = substring.split("<a href=\"#10\">")[1];
+                    substring = substring.split("<a href=\"#10\">")[0];
                 }
 
-                String substringDate = inhaltSplitted[n+1];
+                String substringDate = inhaltSplitted[lineOfCodeFromWeb + 1];
                 String date = substringDate.split("</div>")[0];
-                date = date.split("</div>")[1];
+                //System.out.println(date);
+
+                //date = date.split("</div>")[1];
+                date = date.replace("</div>", "");
+                //System.out.println(date);
 
                 //parsing date
-                if(!date.contains("wöchentlich")){
+                if (!date.contains("wöchentlich")) {
                     date = date.split(" ")[1];
                     date = date.split(" ")[0];
 
-                    if(date.contains(".22")){
+                    if (date.contains(".22")) {
                         date = date.split(".22")[0];
-                    }
-                    else if(date.contains(".23")){
+                    } else if (date.contains(".23")) {
                         date = date.split(".23")[0];
-                    }
-                    else if(date.contains(".24")){
+                    } else if (date.contains(".24")) {
                         date = date.split(".24")[0];
                     }
-                }else {
-                    if (date.contains("Mo")){
+
+                    System.out.println("DATE:\n" + date);
+                } else {
+                    if (date.contains("Mo")) {
                         date = dateMo;
                     } else if (date.contains("Di")) {
                         date = dateDi;
@@ -148,7 +183,11 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 //parsing weekday
-                if(date.equals(dateMo)){
+                if(date.contains(" ")){
+                    date = date.split(" ")[1];
+                }
+                System.out.println("---" + date + "---");
+                if (date.equals(dateMo)) {
                     weekday = "Mo";
                 } else if (date.equals(dateDi)) {
                     weekday = "Di";
@@ -164,10 +203,17 @@ public class MainActivity extends AppCompatActivity {
                 place = place.split("<strong>")[1];
                 place = place.split("</strong>")[0];
 
+                System.out.println();
+                System.out.println(weekday);
+                System.out.println(name);
+                System.out.println(place);
+                System.out.println(date);
+                System.out.println(starttime + " - " + endtime);
+
+
                 vorlesungen.put(vorlesungen.size()+1, new vorlesung(weekday, name, place, date, starttime, endtime));
 
             }
         }
-
     }
 }
