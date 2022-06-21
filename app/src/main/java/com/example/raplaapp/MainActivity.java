6e865b485499a55462a1ba97d761ab9168ba255d
hanese <hannes.schoeller@t-online.de>
2022-06-21 13:24:29 +0200

    input feld und submit button für rapla link erstellt. Submit button ruft Browser mit Stundenplan auf

--- app/src/main/java/com/example/raplaapp/MainActivity.java
+++ app/src/main/java/com/example/raplaapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.raplaapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button submitURLButton;
+    EditText insertURLText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitURLButton = findViewById(R.id.submitURL);
+        insertURLText = findViewById(R.id.inputRaplaLink);
+        submitURLButton.setOnClickListener(view -> {
+            String url = insertURLText.getText().toString();
+            if (!url.startsWith("https://")) {
+                url = "https://" + url;
+            }
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(browserIntent);
+        });
     }
 }
